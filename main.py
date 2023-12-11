cfc18541895d4240c51e8b6937525abda8e0da7b
alyssrod <125409694+alyssrod@users.noreply.github.com>
2023-12-10 20:18:37 -0500

    Update main.py

--- main.py
+++ main.py
@@ -8,86 +8,86 @@ import json
 import base64
 from bs4 import BeautifulSoup 
 import requests
-from setlistdata import *
-from songlinkdata import *
-from spotifydata import *
+from spotifydata import get_spotify_token, spotify_artist_search, spotify_get_songs_by_artist
 
-#sets up main id tables
-cur, conn = setUpDatabase('final-database.db')
-cur.execute("CREATE TABLE IF NOT EXISTS artist_ids(id INTEGER primary key, artist_name UNIQUE)")
-cur.execute("CREATE TABLE IF NOT EXISTS song_ids(id INTEGER primary key, song_name UNIQUE)")
-conn.commit()
+conn = sqlite3.connect('final-database.db')
+cur = conn.cursor()
 
-#selects artist from a list and checks if already in artist id table 
-artist_to_search=["Beyoncé", "Taylor Swift", "Hozier", "Ed Sheeran", "BTS", "Matt Maeson", "Olivia Rodrigo", "Conan Gray", "Lovejoy", "Joji"]
-for x in range(len(artist_to_search)):
-    artist=artist_to_search[x] #add method to check if artist is in id table and continue if is
-    cur.execute("SELECT artist_name FROM artist_ids")
-    artist_list=cur.fetchall()
-    current_artists=[]
-    for item in artist_list:
-        current_artists.append(item[0])
-    if set(artist_to_search).issubset(current_artists):
-        print("All artists updated!")
-        exit()
-    if artist not in current_artists:
-        break
+def get_setlists_for_artists(api_key, artist):
+    base_url = "https://api.setlist.fm/rest/1.0/search/setlists"
+    headers = {
+        'Accept': 'application/json',
+        'x-api-key': 'E0Wf7sQUC93BOCSNEfplxwC9CXC9rQ84_CTh'
+    }
+
+    all_setlists = []
+
+    params = {
+        'artistName': artist
+        }
+
+    response = requests.get(base_url, params=params, headers=headers)
+
+    if response.status_code == 200:
+        setlists = response.json()['setlist']
+        all_setlists.extend(setlists)
     else:
-        continue 
+        print(f"Error getting setlists for {artist}: {response.content}")
+
+    return all_setlists
+
+def store_setlists_for_artists(data,track_list,cur,conn):
+
+    song_count_dict={}
+
+    cur.execute("CREATE TABLE IF NOT EXISTS setlist_song_data (artist_id INTEGER, song_id INTEGER, song_occurences INTEGER)")
+    for item in data: 
+        artist=item["artist"]["name"]
+        try:
+            query="SELECT id FROM artist_ids WHERE artist_name= ?" 
+            cur.execute(query, (artist,))
+            artist_id=cur.fetchone()
+            artist_id=artist_id[0]
+        except:
+            continue
+        all_sets=item["sets"]
+        for set in all_sets:
+            for song in all_sets["set"]:
+                for item in song["song"]:
+                    final_song=item["name"]  
+                    song_count_dict[final_song]=song_count_dict.get(final_song,0)+1
         
-#load lists for songlink and setlist and get top 10 songs from selected artist
-songlink_urls=[]
-songlink_titles=[]
-spotify_track_list=[]
+    potential_track_list=track_list
 
-token = get_spotify_token()
-artist_info = spotify_artist_search(token, artist)
-if artist_info:
-    songs = spotify_get_songs_by_artist(token, artist_info)
-    if songs:
-        for song in songs:
-            songlink_input=(song["external_urls"]["spotify"])
-            songlink_urls.append(songlink_input)
-            spotify_track_list.append(song)
-            songlink_titles.append(song["name"])
-            song_artist=song['artists'][0]['name']
-            cur.execute("INSERT OR IGNORE INTO artist_ids (artist_name) VALUES (?)", (song_artist,))
-            conn.commit()
-    else:
-        print("No tracks found for this artist.")
-else:
-    print("No artist found with this name.")
-
-
-
-#get all song ids from spotify top 10 list
-def add_song_id_table(song,cur,conn):
-    cur.execute("CREATE TABLE IF NOT EXISTS song_ids(id INTEGER primary key, song_name UNIQUE)")
-    song_name=song
-    cur.execute("INSERT OR IGNORE INTO song_ids (song_name) VALUES (?)", (song_name,))
+    for track in potential_track_list: 
+        song_title = track
+        song_count= song_count_dict.get(song_title,0)
+        query="SELECT id FROM song_ids WHERE song_name= ?"
+        cur.execute(query, (song_title,))
+        song_id=cur.fetchone()
+        song_id=song_id[0]
+        cur.execute("INSERT INTO setlist_song_data (artist_id,song_id, song_occurences) VALUES (?,?, ?)", (artist_id,song_id, song_count))
     conn.commit()
+    return None
 
-#adds song to song id table
-for item in spotify_track_list:
-    add_song_id_table(item["name"], cur,conn)
-
-#stores data for spotify tracks in table 
-for song in spotify_track_list:
-    store_spotify_data(song, cur, conn)
-
-#stores songlink data
-songlink_results=[]
-index=0
-for item in songlink_urls:
-    title=songlink_titles[index]
-    result=get_songlink_data(item,title)
-    songlink_results.append(result)
-    index+=1
-store_songlink_data(songlink_results, cur, conn)
-
-#stores both setlist tables
-api_key = 'E0Wf7sQUC93BOCSNEfplxwC9CXC9rQ84_CTh'
-artists_to_search = artist
-result_setlists = get_setlists_for_artists(api_key, artists_to_search)
-store_setlists_for_artists(result_setlists,songlink_titles,cur,conn)
-store_tours_for_artists(result_setlists,cur,conn)
+def store_tours_for_artists(data,cur,conn):
+    limiter=0
+    cur.execute("CREATE TABLE IF NOT EXISTS setlist_data (artist_id INTEGER, date TEXT, venue TEXT)")
+    for item in data:
+        if limiter > 25:
+            break
+        else:
+            try:
+                artist=item["artist"]["name"]
+                query="SELECT id FROM artist_ids WHERE artist_name= ?" 
+                cur.execute(query, (artist,))
+                artist_id=cur.fetchone()
+                artist_id=artist_id[0]
+                date=item["eventDate"]
+                venue=item["venue"]["name"]
+                cur.execute("INSERT INTO setlist_data (artist_id,date,venue) VALUES (?, ?,?)", (artist_id,date,venue))
+                limiter+=1
+            except:
+                continue
+    conn.commit()
+    return None
